4e7ccff1cfcedd441ec1fb91e8360ee78ef8f266
Kris Popendorf <krispop@google.com>
2014-07-17 02:11:45 +0900

    fun

--- myapp/hello.go
+++ myapp/hello.go
@@ -1,19 +1,48 @@
 package hello
 
 import (
-    "fmt"
-    "net/http"
+  "fmt"
+  "net/http"
 )
 
 func init() {
-    http.HandleFunc("/", root)
-    http.HandleFunc("/recv", recv)
+  http.HandleFunc("/", root)
+  http.HandleFunc("/recv", recv)
+  http.HandleFunc("/convert", recv)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+	fmt.Fprint(w, "Hello, world!")
 }
 
 func recv(w http.ResponseWriter, r *http.Request) {
-     fmt.Fprint(w, r.FormValue("content"));
+	var v = r.FormValue("content")
+	if (len(v)<1) {
+		v = r.FormValue("message")
+	}
+	var k = Atoi(r.FormValue("k"))
+	if (k < 1) {
+		k = 3
+	}
+	for (int ik = 0; ik < k; ++ik) {
+		for (int i = 0; i < len(v);) {
+			if (ik == 0) {
+				fmt.Print(w, v[i]);
+				i += (k-1) * 2;
+			} else if (ik > 0 && ik < k-1) { // middle
+				i += ik;
+				if (i < len(v)) {
+					fmt.Fprint(w, v[i]);
+				}
+				i += 2 * (k - ik)  // go to bottom and come back
+				if (i < len(v)) {
+					fmt.Fprint(w, v[i])
+				}
+			} else { // bottom
+				i += ik;
+				fmt.Fprint(w, [i]);
+				i += ik;
+			}
+		}
+	}
 }
