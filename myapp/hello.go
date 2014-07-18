a97876e4884b6d0ecaf41d4665f41e2baf1a250d
Kris Popendorf <krispop@google.com>
2014-07-18 13:31:11 +0900

    fun

--- myapp/hello.go
+++ myapp/hello.go
@@ -20,12 +20,15 @@ func init() {
 	http.HandleFunc("/send", send)
 	http.HandleFunc("/show", send)
 	http.HandleFunc("/getword", getword)
+	http.HandleFunc("/madlib", madlib)
 }
 
 var kPeers = []string {
 	"http://1-dot-step-homework-hnoda.appspot.com/stephomeworkhnoda",
 	"http://step-test-krispop.appspot.com"}
 
+var kMadlibPeers = []string {
+	"http://step-test-krispop.appspot.com"}
 
 func root(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello, world!")
@@ -102,10 +105,14 @@ func send(w http.ResponseWriter, r *http.Request) {
 
 func FetchUrl(url string, c appengine.Context, cs chan string) {
   client := urlfetch.Client(c)
+	c.Infof("Fetching URL: %s", url)
 	resp, err := client.Get(url)
 	if (err == nil) {
 		body, _ := ioutil.ReadAll(resp.Body)
-		cs <- fmt.Sprintf("%s => %s", url, string(body))
+		c.Infof("Success getting URL: %s", url)
+//		cs <- fmt.Sprintf("%s => %s", url, string(body))
+		cs <- string(body)
+		c.Infof("Passed channel inject for %s", url)
 	} else {
 		c.Infof("Error fetching %s => %s", url, err)
 		cs <- fmt.Sprintf("[Error: %s]")
@@ -117,6 +124,7 @@ func getword(w http.ResponseWriter, r *http.Request) {
 	nouns := []string {"banjo", "drum stick", "pine cone", "pretzle"}
 	adjectives := []string {"bright", "tasty", "squiggly"}
 	animals := []string {"weasel", "unicorn", "dragon", "lemur"}
+	names := []string {"Boris", "Shiina", "Chrono", "Hermione"}
 	
 	var word string
 	switch(r.FormValue("pos")) {
@@ -132,6 +140,9 @@ func getword(w http.ResponseWriter, r *http.Request) {
 	case "animal":
 		word = PickRandom(animals)
 		break
+	case "name":
+		word = PickRandom(names)
+		break
 	default:
 		word = PickRandom(append(append(append(adjectives, animals...), nouns...), verbs...))
 	}
@@ -142,3 +153,33 @@ func PickRandom(choices []string) string {
 	return choices[rand.Intn(len(choices))]
 }
 
+func GetRandomWord(pos string, c appengine.Context) chan string {
+	url := fmt.Sprintf("%s/getword?pos=%s", PickRandom(kMadlibPeers), pos)
+	cs := make(chan string, 1)
+	FetchUrl(url, c, cs)
+	return cs
+}	
+
+func madlib(w http.ResponseWriter, r *http.Request) {
+  c := appengine.NewContext(r)
+	gw := func(pos string) chan string {return GetRandomWord(pos, c)}
+	w.Header().Set("Content-Type", "text/plain")
+	res := ""
+	switch (rand.Intn(3)) {
+	case 0:
+		ws := []chan string {gw(""), gw("adjective"), gw("animal"), gw("noun"), gw("adjective")}
+		res = fmt.Sprintf(`The mayor of %s-Town was a %s %s. One day the mayor ate a %s and said it was very %s!`, <-ws[0], <-ws[1], <-ws[2], <-ws[3], <-ws[4]);
+		break
+	case 1:
+		ws := []chan string {gw("adjective"), gw("name"), gw("adjective"), gw("animal")}
+		res = fmt.Sprintf(`"My what %s teeth you have" said %s the %s %s.`, <-ws[0], <-ws[1], <-ws[2], <-ws[3])
+		break
+	case 2:
+		ws := []chan string {gw("noun"), gw("name"), gw("noun"), gw("animal")}
+		res = fmt.Sprintf(`"Now where did I put my %s...?" said %s, while moving the %s before it was eaten by the wild %s.`, <-ws[0], <-ws[1], <-ws[2], <-ws[3])
+		break
+	default:
+		panic("Impossible")
+	}
+	fmt.Fprint(w, res)
+}
