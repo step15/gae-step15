4b9a88572ed0858f5cede5a2f9ebaea9d1ab41aa
Kris Popendorf <krispop@google.com>
2015-07-13 20:22:26 +0900

    silly parse

--- myapp/magic.go
+++ myapp/magic.go
@@ -5,12 +5,24 @@ import (
 	"strings"
 )
 
-var bodyRE = regexp.MustCompile("<body>([^<]+)")
+var bodyRE = regexp.MustCompile("(?is:<body>(.+))")
+var tagRE = regexp.MustCompile("(?is:<[^>]*>)")
+
+func dropTags(html string) string {
+	r := ""
+	for _, x := range tagRE.Split(html, -1) {
+		r += x + " "
+	}
+	return r
+}
 
 func StripTags(maybeHtml string) string {
 	r := maybeHtml
 	if matches := bodyRE.FindStringSubmatch(maybeHtml); len(matches) > 1 {
 		r = matches[1]
 	}
-	return strings.TrimSpace(r)
+	r = dropTags(r)
+	r = strings.Replace(r, "\n", "", -1)
+	r = strings.TrimSpace(r)
+	return r
 }
--- myapp/magic_test.go
+++ myapp/magic_test.go
@@ -16,3 +16,28 @@ mysterious
 	fmt.Println(StripTags(html))
 	// Output: mysterious
 }
+
+func ExampleStripLotsOfTags() {
+	html := `
+<html>
+<head>
+<title>getword</title>
+</head>
+<body>
+<h1>Your word is</h1>
+mysterious
+</body>
+</html>`
+	fmt.Println(StripTags(html))
+	// Output: Your word is mysterious
+}
+
+func ExampleDropTagsBoring() {
+	fmt.Println(dropTags("boring"))
+	// Output: boring
+}
+
+func ExampleDropTags() {
+	fmt.Println(dropTags("please<p>drop<p>me<p>"))
+	// Output: please drop me
+}
